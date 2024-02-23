103df968ee2f0541594abe006eb59a788fdbfc30
Johhny <jhasse20@hotmail.com>
2024-02-22 21:52:54 -0300

    add default port

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { config } from "dotenv";
 
 config();
 
+console.log(".ENV API PORT: ", process.env.API_PORT);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.API_PORT);
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
